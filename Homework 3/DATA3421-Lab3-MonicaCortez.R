8f3679a332e56b126acc327446078703c1164b80
monicacortez <89665169+monicacortez@users.noreply.github.com>
2023-02-23 21:31:19 -0600

    Add files via upload

---
+++ Homework 3/DATA3421-Lab3-MonicaCortez.R
@@ -0,0 +1,73 @@
+#install.packages("tidyverse")
+library(tidyverse)
+#install.packages("dplyr") 
+library(dplyr)
+#install.packages("tidyr")
+library(tidyr)
+#install.packages("ggplot2") 
+library(ggplot2) 
+#install.packages("moments")
+library(moments)
+
+#Question 1. Creating a matrix with three 12 and 8 columns and calculating the means for each column of the matrix?
+
+a<-matrix(1:36, 3, 12, byrow=TRUE)
+a 
+apply(a,2,mean)
+
+#Question 2. Calculate sum, mean, median, standard deviation, skewness, quantile, kurtosis, and variance for variable “Balance” in the job dataset.
+
+df<-read.csv("/Users/monicacortez/Downloads/job.csv")
+df
+
+sum(df$Balance)
+mean(df$Balance)
+median(df$Balance)
+sd(df$Balance)
+skewness(df$Balance)
+quantile(df$Balance)
+kurtosis(df$Balance)
+var(df$Balance)
+
+#Question 3.Recode the “Age” variable to three categories of “Young Adult”, “Middle-aged Adult”, and “Old Adult”, based on quantiles of 30%, 60%, and 80%.
+quantile(df$Age,prob=c(0.3,0.6,0.8))
+df$New_Age[df$Age<=33]="Young Adult"
+df$New_Age[df$Age>33&df$Age<=40]="Middle-Aged Adult"
+df$New_Age[df$Age>40]="Old Adult"
+view(df)
+
+#Question 4.Sort the “Balance” variable from high to low in the job dataset and create a new subset of the dataset with just Balance and Job Classification variables.
+
+df[order(df$Balance,decreasing=TRUE),]
+df_subset<-df[, c("Balance", "Job.Classification")]
+df_subset
+
+#Question 5.Subset the job dataset with just the England region and with a balance of higher than 100,000
+
+df_subsetregion<-df[df$Region=="England", ]
+df_subsetengland<-df_subsetregion[df_subsetregion$Balance>100000, ]
+df_subsetengland
+
+#Question 6.Using the flights dataset, create a new variable using existing variables (arr_time and sched_arr_time) and call it delay time.
+
+install.packages("nycflights13")
+library(nycflights13)
+View(flights)
+
+flights<-mutate(flights,delay_time=arr_time-sched_arr_time)
+View(flights)
+
+
+
+
+
+
+
+
+
+         
+         
+         
+         
+         
+         
